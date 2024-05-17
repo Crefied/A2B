89aab78913f0763d67ae5f7a10b5d9ac35700fe2
withoutLeave <147837334+withoutLeave@users.noreply.github.com>
2024-05-17 22:41:34 +0800

    Update main.cpp
    test

--- main.cpp
+++ main.cpp
@@ -4,15 +4,19 @@
 #include <QLocale>
 #include <QTranslator>
 
+////
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+    for (const QString &locale : uiLanguages)
+    {
         const QString baseName = "test_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
+        if (translator.load(":/i18n/" + baseName))
+        {
             a.installTranslator(&translator);
             break;
         }
