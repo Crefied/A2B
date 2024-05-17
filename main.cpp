415d37266b2c85a17353a74b92b1de76c022a85f
withoutLeave <147837334+withoutLeave@users.noreply.github.com>
2024-05-17 22:35:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <QApplication>
 #include <QLocale>
 #include <QTranslator>
+//
 
 int main(int argc, char *argv[])
 {
@@ -10,9 +11,11 @@ int main(int argc, char *argv[])
 
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
-    for (const QString &locale : uiLanguages) {
+    for (const QString &locale : uiLanguages)
+    {
         const QString baseName = "test_" + QLocale(locale).name();
-        if (translator.load(":/i18n/" + baseName)) {
+        if (translator.load(":/i18n/" + baseName))
+        {
             a.installTranslator(&translator);
             break;
         }
