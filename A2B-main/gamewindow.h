36b73a853585e3038b8918252909d439563c6324
Affection0824 <taoyetage@outlook.com>
2024-05-29 16:10:20 +0800

    Add files via upload

---
+++ A2B-main/gamewindow.h
@@ -0,0 +1,33 @@
+#ifndef GAMEWINDOW_H
+#define GAMEWINDOW_H
+
+#include <QWidget>
+#include<QPointer>
+#include "system.h"
+namespace Ui {
+class GameWindow;
+}
+
+class GameWindow : public QWidget
+{
+    Q_OBJECT
+
+public:
+    GameWindow(QWidget *parent = nullptr);
+    ~GameWindow();
+    Ui::GameWindow* ui;
+
+private slots:
+    void on_run_clicked();
+
+    //void on_backButton_clicked();
+
+private:
+
+    System * system;
+    Stage * stage;
+};
+
+
+
+#endif // GAMEWINDOW_H
