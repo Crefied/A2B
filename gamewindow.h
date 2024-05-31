10e86cc3a8ea31d0746e49851c65b75a7eb5c12e
Stellarism <109578201+Crefied@users.noreply.github.com>
2024-05-31 15:10:27 +0800

    Add files via upload

--- gamewindow.h
+++ gamewindow.h
@@ -17,35 +17,17 @@ public:
     GameWindow(QWidget *parent = nullptr);
     ~GameWindow();
     Ui::GameWindow *ui;
+
     Stage *stage;
-    bool isRunning;
+
     void setStageInfo(); // 已经更新stage信息才调用
-    void updateOutputInfo(const QString & info,bool clear); // info 是输入信息，clear 是清屏
-    void threadEnd();
 private slots:
     void on_run_clicked();
 
-    void on_pause_clicked();
-
-    void on_start_clicked();
-
-    void on_slow_clicked();
-
-    void on_quick_clicked();
-
-    void on_stop_clicked();
-
-signals:
-    void startThread(Stage * _stage,QTextDocument * _input,bool _debug);
-    void speedChange(bool adjust);
-    void stop();
-    void pause();
-    void resume();
     // void on_backButton_clicked();
 
 private:
-    System * system;
-    QThread * thread;
+    System *system;
 };
 
 #endif // GAMEWINDOW_H
