f73169f1e15d3e9163ced454541ed8691b39511a
withoutLeave <147837334+withoutLeave@users.noreply.github.com>
2024-05-31 18:47:05 +0800

    删除文件，debug,调整GUI

--- mainwindow.h
+++ mainwindow.h
@@ -4,38 +4,40 @@
 
 #include <QMainWindow>
 #include "gamewindow.h"
+#include"gamewindow_designed.h"
 #include<QStackedLayout>
 #include<qstackedwidget.h>
 #include<QPointer>
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
+namespace Ui
+{
+    class MainWindow;
 }
 QT_END_NAMESPACE
 
 
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     QWidget* centralWidget;
-	QStackedWidget* stackedWidget;
+    QStackedWidget* stackedWidget;
     QString styleSheet;
 
-    Stage* stages[10][10] = {nullptr};
+    Stage* stages[10][10] = { nullptr };
     bool stageAble[10][10] = { false };
 
     //MainWindow(QWidget *parent = nullptr);
-    MainWindow(GameWindow * _gamewindow,QWidget *parent = nullptr);
+    MainWindow(GameWindow* _gamewindow, QPointer<gamewindow_designed> _designedWindow, QWidget* parent = nullptr);
     ~MainWindow();
 
 private slots:
     void on_pushButton_clicked();
 
 private:
-    Ui::MainWindow *ui;
-	QPointer<GameWindow> gamewindow;
+    Ui::MainWindow* ui;
+    QPointer<GameWindow> gamewindow;
+    QPointer<gamewindow_designed> designedWindow;
 };
 #endif // MAINWINDOW_H
