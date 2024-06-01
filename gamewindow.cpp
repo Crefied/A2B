02aa3092bf4cae6bb5bad6cf82a8e6a8b01fb25e
Stellarism <109578201+Crefied@users.noreply.github.com>
2024-06-01 11:24:28 +0800

    修复了重启bug

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -1,4 +1,4 @@
-#include "gamewindow.h"
+﻿#include "gamewindow.h"
 #include "ui_gamewindow.h"
 
 GameWindow::GameWindow(QWidget *parent)
@@ -6,21 +6,113 @@ GameWindow::GameWindow(QWidget *parent)
     , ui(new Ui::GameWindow)
 {
     ui->setupUi(this);
-    system = new System();
+    system = NULL;
     stage = new Stage();
+    thread = NULL;
+
+    isRunning = false;
+    connect(ui->upload, &QPushButton::clicked, this, [=]()
+        {
+            this->stage->saveStage();
+        });
 }
 
 GameWindow::~GameWindow()
 {
     delete ui;
-    delete system;
     delete stage;
 }
 
 void GameWindow::on_run_clicked()
 {
-    stage->answer = ui->view->document();
-    System::logWidget = ui->textBrowser;
-    system->run(stage,ui->IDE->document(),false);
+    if(!isRunning)
+    {
+        system = new System();
+        thread = new QThread(this);
+        system->moveToThread(thread);
+        connect(this,&GameWindow::startThread,system,&System::run); // 计算关联
+        connect(system,&System::updateShow,this,&GameWindow::updateOutputInfo); // 文本输出关联
+        connect(system,&System::CalEnd,this,&GameWindow::threadEnd, Qt::DirectConnection);
+        connect(thread,&QThread::finished,thread,&QThread::deleteLater, Qt::DirectConnection);
+        connect(thread,&QThread::finished,system,&QObject::deleteLater, Qt::DirectConnection);
+        stage->answer = ui->view->document(); // 设置答案
+        thread->start(); // 线程开始
+        //connect(this,&GameWindow::resume,system,&System::resume, Qt::QueuedConnection);
+        //connect(this,&GameWindow::stop,system,&System::stop, Qt::QueuedConnection);
+        //connect(this,&GameWindow::pause,system,&System::pause, Qt::QueuedConnection);
+        //connect(this,&GameWindow::speedChange,system,&System::speedChange, Qt::QueuedConnection);
+        emit startThread(stage,ui->IDE->document(),false); // 开始计算
+        ui->Progress->resize(611,31);
+        isRunning = true;
+    }
+}
+
+
+//void GameWindow::on_backButton_clicked()
+
+
+
+void GameWindow::setStageInfo()
+{
+	ui->Puzzle->appendPlainText(stage->name);
+	ui->Puzzle->appendPlainText(stage->scriptDescription);
+	ui->Puzzle->appendPlainText(stage->caseDescription);
+}
+
+void GameWindow::updateOutputInfo(const QString & info,bool clear)
+{
+    if(clear)
+    {
+        ui->Output->clear();
+    }
+    ui->Output->append(info);
+}
+void GameWindow::threadEnd()
+{
+    thread->quit();
+    thread->wait();
+    isRunning = false;
+}
+
+void GameWindow::on_pause_clicked()
+{
+    if(thread->isRunning())
+        system->isPause = true;
+}
+
+
+
+void GameWindow::on_start_clicked()
+{
+    if(thread->isRunning())
+    {
+        QMutexLocker locker(&system->mutex);
+        system->isPause = false;
+        system->condition.wakeAll();
+    }
+}
+
+
+void GameWindow::on_slow_clicked()
+{
+    if(thread->isRunning())
+        system->speed = std::min(0,system->speed - 1);
+}
+
+
+void GameWindow::on_quick_clicked()
+{
+    if(thread->isRunning())
+        system->speed = std::max(2,system->speed + 1);
+}
+
+
+void GameWindow::on_stop_clicked()
+{
+    if(thread->isRunning())
+    {
+        system->isStop = true;
+        ui->Output->clear();
+    }
 }
 
--- gamewindow.h
+++ gamewindow.h
@@ -1,10 +1,12 @@
-#ifndef GAMEWINDOW_H
+﻿#ifndef GAMEWINDOW_H
 #define GAMEWINDOW_H
 
 #include <QWidget>
+#include <QPointer>
 #include "system.h"
-namespace Ui {
-class GameWindow;
+namespace Ui
+{
+    class GameWindow;
 }
 
 class GameWindow : public QWidget
@@ -14,16 +16,36 @@ class GameWindow : public QWidget
 public:
     GameWindow(QWidget *parent = nullptr);
     ~GameWindow();
-
+    Ui::GameWindow *ui;
+    Stage *stage;
+    bool isRunning;
+    void setStageInfo(); // 已经更新stage信息才调用
+    void updateOutputInfo(const QString & info,bool clear); // info 是输入信息，clear 是清屏
+    void threadEnd();
 private slots:
     void on_run_clicked();
 
+    void on_pause_clicked();
+
+    void on_start_clicked();
+
+    void on_slow_clicked();
+
+    void on_quick_clicked();
+
+    void on_stop_clicked();
+
+signals:
+    void startThread(Stage * _stage,QTextDocument * _input,bool _debug);
+    void speedChange(bool adjust);
+    void stop();
+    void pause();
+    void resume();
+    // void on_backButton_clicked();
+
 private:
-    Ui::GameWindow *ui;
     System * system;
-    Stage * stage;
+    QThread * thread;
 };
-
-
 
 #endif // GAMEWINDOW_H
